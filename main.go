2d65e350bc5abd094e56e73f1791f1f567b96c75
Jarkko Rantamäki <jarkko.rantamaki@iki.fi>
2017-02-23 01:58:13 +0200

    game over and detect explosions

--- main.go
+++ main.go
@@ -12,12 +12,12 @@ import (
 	"math/rand"
 )
 
-const empty = 0;
 const bullet = 1;
 const monster = 2;
 const explosion = 5;
-
-const ticker_sleep = 400;
+const mayhem = -1
+const ticker_sleep = 200;
+const monsterUpdateModulo = 2;
 
 func main() {
 	tickChan := time.NewTicker(time.Millisecond * ticker_sleep).C
@@ -50,18 +50,23 @@ func main() {
 	world := [][]int{zero, zero, zero, zero, zero, zero, zero, zero}
 	x := 7
 	y := 7
-
-	z := 0;
+	z := -1;
 	for {
 		select {
 		case <-tickChan:
 			fmt.Println("Updating the world! ")
-			updateWorld(world, fb)
+			gameOver := updateWorld(world, z)
+			if gameOver {
+				itsGameOver(world)
+				drawWorld(x, world, fb)
+				time.Sleep(time.Second * 3)
+				screen.Clear()
+				return
+			}
 			z = z + 1
-			if z % 5 == 0 {
+			if z % 8 == 0 {
 
 				monsterX := rand.Int() % 8
-				monsterX = 0
 				tmp := []int{0,0,0,0,0,0,0,0};
 				copy(tmp, world[monsterX]);
 				tmp[0] = monster
@@ -70,7 +75,7 @@ func main() {
 			}
 
 		case <-signals:
-			fmt.Println("")
+			fmt.Println("EXIT")
 			screen.Clear()
 
 			// Exit the loop
@@ -105,21 +110,35 @@ func main() {
 	}
 }
 
-func updateWorld(world [][]int, fb *screen.FrameBuffer) {
+func updateWorld(world [][]int, z int) bool {
 
 	for x := 0; x <= 7; x ++  { // iterate the x-axis
  		oldY := world[x];
-		newY := []int{0,0,0,0,0,0,0,0};
-		for  y := 0 ;  y <= 7 ; y ++ { // move down the monsters in the matrix.
-			if y == 7 {
-				break;
-			}
-			if oldY[y] == monster {
-				newY[y+1] = monster
-			}
+
+		if oldY[7] == monster {
+			return true  // GAME OVER
 		}
-		if x == 0{
-			fmt.Println("Moved the monsters  ", oldY, " newY ", newY)
+
+		newY := []int{0,0,0,0,0,0,0,0};
+		if z % monsterUpdateModulo == 0 { // hack to manage the monster vs bullet speed.
+			for  y := 0 ;  y <= 7 ; y ++ { // move down the monsters in the matrix.
+				if y == 7 {
+					break;
+				}
+				if oldY[y] == monster {
+					newY[y+1] = monster
+				}
+			}
+			if x == 0{
+				fmt.Println("Moved the monsters  ", oldY, " newY ", newY)
+			}
+		} else {
+			copy(newY, oldY)
+			for y := 0 ; y <= 7 ; y ++ {
+				if newY[y] == bullet {
+					newY[y] = 0;
+				}
+			}
 		}
 
 		// Was there any collisions with the bullets?
@@ -131,25 +150,35 @@ func updateWorld(world [][]int, fb *screen.FrameBuffer) {
 				oldY[y] = 0;
 			}
 		}
-
+		newMonsters := []int{0,0,0,0,0,0,0,0};
+		copy(newMonsters, newY)
 
 		for  y := 6 ; y > 0; y-- { // move the bullets
 			if y == 0 {
 				break;
 			}
-			if oldY[y]  == bullet && newY[y] == monster {
+			if oldY[y]  == bullet && newMonsters[y] == monster {
+				fmt.Println("1. Detected Explosion!! newMonsters: ", newMonsters , " oldY ", oldY )
+
 				newY[y] = explosion
 
-			} else if oldY[y] == bullet {
-				newY[y-1] = bullet
+			} else if oldY[y] == bullet && newMonsters[y-1] == monster{
+				fmt.Println("2. Detected Explosion!! newMonsters: ", newMonsters , " oldY ", oldY )
+				newY[y-1] = explosion
+			} else {
+				if oldY[y] == bullet {
+					newY[y-1] = bullet
+				}
 			}
 		}
 		if x == 0 {
-			fmt.Println("Moved the Bullets:  ", oldY, " newY ", newY)
+			fmt.Println("Moved the Bullets:  ", oldY, " newY ", newY, " newMonsters ", newMonsters)
 		}
+
 		world[x] = newY;
 
 	}
+	return false
 }
 
 
@@ -158,23 +187,26 @@ func drawWorld(spaceship int, world [][]int, fb *screen.FrameBuffer) {
 
 		for  y:= 0 ; y <=  7; y++ {
 			if world[x][y]  == monster {
-				fmt.Println("Rendering a monster x:" ,x, " y: ",y)
 				fb.SetPixel(x, y, color.Red)
-
 			} else if world[x][y]  == bullet {
-				fmt.Println("Rendering a bullet x:" ,x, " y: ",y)
 				fb.SetPixel(x, y, color.Blue)
 			} else if world[x][y]  == explosion {
-				fmt.Println("Rendering a explosion x:" ,x, " y: ",y)
 				fb.SetPixel(x, y, color.New(255,255,0))
+
+			} else if world[x][y]  == mayhem {
+				fb.SetPixel(x, y, color.New(244,66,241))
 
 			} else {
 				fb.SetPixel(x, y, color.New(0, 0, 0))
 			}
-
 		}
 	}
 	fb.SetPixel(spaceship, 7, color.Green)
 	screen.Draw(fb)
 }
 
+func itsGameOver(world [][]int) {
+	for x := 0; x <= 7; x ++ {
+		world[x] =  []int{mayhem,mayhem,mayhem,mayhem,mayhem,mayhem,mayhem,mayhem};
+	}
+}
